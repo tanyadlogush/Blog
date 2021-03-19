dbff10e34b743eec9b82f732b30451d9bc58e640
tanyadlogush <tanyadlogush@gmail.com>
2021-03-19 14:46:18 +0200

    Lesson 5-11

--- backend/core/forms.py
+++ backend/core/forms.py
@@ -5,8 +5,11 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        # fields = ['title', 'body', 'image']
+        # fields = ['title', 'body', Post.author, 'image']
         fields = ['title', 'body', 'author', 'image']
+    #
+    # def post_author(self):
+    #     self.data['author'] = Post.author
 
 
     # def clean_title(self):
